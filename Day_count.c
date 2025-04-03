e12882a9f4b892a5f3970486733fb824acb44585
_p0ty1103 <121510825+Pochi-Liberluna@users.noreply.github.com>
2025-04-03 17:43:10 +0900

    Update Day_count.c

--- Day_count.c
+++ Day_count.c
@@ -20,5 +20,25 @@ int main(void){
   printf("西暦の年数を入力してください: ");
   scanf("%d\n", &year);
 
-  uru();
+  if(uru(year) == 1){
+    printf("うるう年として数えます。\n");
+    days[2] = 29;
+  } else{
+    printf("うるう年でないとして数えます。\n");
+    days[2] = 28;
+}
+do{
+  printf("9月1日=0901のように、月日を4けたで入力してください:");
+  scanf("%d", %mmdd);
+  m = mmdd / 100;
+  d = mmdd % 100;
+} while(m < 1 || 12 < m || d < 1 || days[m] < d); //異常があれば再入力
+ans = 0;
+  for(i = 0;i < m;i++){
+    ans = ans + d[i];
+  }
+  ans = ans + d;
+
+  printf("%d月%d日は、1月1日を第1日目として、%d日目です。\n", m, d, ans);
+  return 0;
 }
